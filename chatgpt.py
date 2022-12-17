c9530ee00da6c97e73a3d7de6d1677ca1515329c
JokerWuXin <41312693+JokerWuXin@users.noreply.github.com>
2022-12-17 20:05:42 +0800

    Add files via upload

--- chatgpt.py
+++ chatgpt.py
@@ -3,7 +3,7 @@ from api.prompt import Prompt
 import os
 import openai
 
-openai.api_key = os.getenv("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")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 class ChatGPT:
