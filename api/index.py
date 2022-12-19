fbd98c68c4b7f1f276834a4d79dab540b2fe3844
JokerWuXin <41312693+JokerWuXin@users.noreply.github.com>
2022-12-19 21:54:33 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -40,7 +40,19 @@ def handle_message(event):
     if event.message.type != "text":
         return
     
-    working_status = True
+    if event.message.text == "啟動":
+        working_status = True
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="我是時下流行的AI智能，目前可以為您服務囉，歡迎來跟我互動~"))
+        return
+
+    if event.message.text == "安靜":
+        working_status = False
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="感謝您的使用，若需要我的服務，請跟我說 「啟動」 謝謝~"))
+        return
     
     if working_status:
         chatgpt.add_msg(f"Human:{event.message.text}?\n")
