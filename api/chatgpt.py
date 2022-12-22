a2977d73b3e3273d263a39826e95663c447144a7
JokerWuXin <41312693+JokerWuXin@users.noreply.github.com>
2022-12-22 12:34:30 +0800

    Update chatgpt.py

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -5,7 +5,7 @@ import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
+text = "123"
 class ChatGPT:
     def __init__(self):
         self.prompt = Prompt()
@@ -17,15 +17,19 @@ class ChatGPT:
         self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 240))
 
     def get_response(self):
-        response = openai.Completion.create(
-            model=self.model,
-            prompt=self.prompt.generate_prompt(),
-            temperature=self.temperature,
-            frequency_penalty=self.frequency_penalty,
-            presence_penalty=self.presence_penalty,
-            max_tokens=self.max_tokens
-        )
-        return response['choices'][0]['text'].strip()
+        try:
+            response = openai.Completion.create(
+                model=self.model,
+                prompt=self.prompt.generate_prompt(),
+                temperature=self.temperature,
+                frequency_penalty=self.frequency_penalty,
+                presence_penalty=self.presence_penalty,
+                max_tokens=self.max_tokens
+            )
+        except openai.api_requests.ApiError:
+            text="Error: API request failed"
+         return text
+        #return response['choices'][0]['text'].strip()
 
     def add_msg(self, text):
         self.prompt.add_msg(text)
