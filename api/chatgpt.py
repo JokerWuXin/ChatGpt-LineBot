7ffa4fa8f129f27c9988f76d573709bef8cd9a11
JokerWuXin <41312693+JokerWuXin@users.noreply.github.com>
2022-12-22 12:27:46 +0800

    Update chatgpt.py

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -8,9 +8,9 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class ChatGPT:
     def __init__(self):
-        #self.prompt = Prompt()
-        self.prompt = "What is the weather like today?"
-        self.model = "chatbot"
+        self.prompt = Prompt()
+        self.model = os.getenv("OPENAI_MODEL", default = "text-davinci-003")
+        #self.model = os.getenv("OPENAI_MODEL", default = "chatbot")
         self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0))
         self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0))
         self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0.6))
