d599f8f5f73f18b443cd2be7e54af4fbece41c63
JokerWuXin <41312693+JokerWuXin@users.noreply.github.com>
2022-12-22 12:01:00 +0800

    Update chatgpt.py

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -9,7 +9,8 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 class ChatGPT:
     def __init__(self):
         self.prompt = Prompt()
-        self.model = os.getenv("OPENAI_MODEL", default = "text-davinci-003")
+        #self.model = os.getenv("OPENAI_MODEL", default = "text-davinci-003")
+        self.model = os.getenv("OPENAI_MODEL", default = "chatbot")
         self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0))
         self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0))
         self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0.6))
@@ -17,15 +18,14 @@ class ChatGPT:
 
     def get_response(self):
         response = openai.Completion.create(
-            #model=self.model,
-            #prompt=self.prompt.generate_prompt(),
+            model=self.model,
+            prompt=self.prompt.generate_prompt(),
             #temperature=self.temperature,
             #frequency_penalty=self.frequency_penalty,
             #presence_penalty=self.presence_penalty,
-            engine="chatbot",
-            prompt=self.prompt.generate_prompt(),
-            max_tokens=20
-            #max_tokens=self.max_tokens
+            top_p=1,
+            temperature=0.5,
+            max_tokens=self.max_tokens
         )
         return response['choices'][0]['text'].strip()
 
