bb04c0547e3720c56f4a3d3968e26fc3a8c3f41e
JokerWuXin <41312693+JokerWuXin@users.noreply.github.com>
2023-05-02 12:32:56 +0800

    Update chatgpt.py
    gpt-3.5-turbo

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -8,7 +8,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 class ChatGPT:
     def __init__(self):
         self.prompt = Prompt()
-        self.model = os.getenv("OPENAI_MODEL", default = "text-davinci-003")
+        self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo")
         #self.model = os.getenv("OPENAI_MODEL", default = "chatbot")
         self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0))
         self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0))
